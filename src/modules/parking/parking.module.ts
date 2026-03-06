15a8ec02c88a6e83456696bb5d2fa6d34abff580
Saurabh Verma <saurabhverma3554@gmail.com>
2026-03-06 17:44:52 +0530

    Making new changes

--- src/modules/parking/parking.module.ts
+++ src/modules/parking/parking.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ParkingController } from './controllers/parking.controller';
+import { FareCalculatorService } from './services/fare-calculator.service';
+import { ParkingLotService } from './services/parking-lot.service';
+import { ParkingManagerService } from './services/parking-manager.service';
 
-@Module({})
+@Module({
+  controllers: [ParkingController],
+  providers: [ParkingLotService, ParkingManagerService, FareCalculatorService],
+  exports: [ParkingLotService],
+})
 export class ParkingModule {}
